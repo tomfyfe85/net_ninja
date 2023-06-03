7fa05c9627f4dfd2eca48fbaf3f2e090c35239c6
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-03 15:52:43 +0100

    10- outputting lists and CSS

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
 const Home = () => {
-  // let name = "mario"
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
+  const [blogs, setBlogs] = useState([
+    { title: "my new website", body: "lorem..", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "orem", author: "yoshi", id: 2 },
+    { title: "web dev to tips", body: "lorem", author: "mario", id: 3 },
+  ]);
 
-  const handleClick = () => {
-    setName("luigi");
-    setAge(100);
-  };
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
