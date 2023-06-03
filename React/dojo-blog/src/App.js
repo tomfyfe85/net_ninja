5ec7bc4f2db12dd5d244f8bb3fd7a4da301d94a7
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-03 12:09:49 +0100

    added css and example of inline styling

--- React/dojo-blog/src/App.js
+++ React/dojo-blog/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Navbar from "./NavBar";
 import Home from "./Home";
 
@@ -7,7 +6,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home/>
+        <Home />
       </div>
     </div>
   );
--- React/dojo-blog/src/NavBar.js
+++ React/dojo-blog/src/NavBar.js
@@ -4,7 +4,12 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">Create New Blog</a>
+      
+        <a href="/create" style={{
+          color: 'white',
+          backgroundColor: '#f1356d',
+          borderRadius: '8px'
+        }}>Create New Blog</a>
 
       </div>
     </nav>
