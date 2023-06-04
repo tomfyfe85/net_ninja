11570cecd73beea443c09805e669478486871bf0
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-04 21:27:17 +0100

    13, added delted function

--- React/dojo-blog/src/BlogList.js
+++ React/dojo-blog/src/BlogList.js
@@ -1,4 +1,6 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
+  
+
   return (
     <div className="blog-list">
       <h1>
@@ -8,6 +10,7 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -2,17 +2,21 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   // const [title, setTitle] = useState()
+
   const [blogs, setBlogs] = useState([
     { title: "my new website", body: "lorem..", author: "mario", id: 1 },
     { title: "Welcome party!", body: "orem", author: "yoshi", id: 2 },
     { title: "web dev to tips", body: "lorem", author: "mario", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title='All Blogs!' />
-      <BlogList blogs={blogs.filter(blog => blog.author === "mario")} title="Mario's Blogs" />
-
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 };
