b48d63d11bdf19c4a67cfe52e1aa6c5d3e779fad
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-04 22:42:01 +0100

    18, using fetch req boolean logic to show loading msg

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -2,24 +2,27 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsLoading(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-{  blogs &&  <BlogList blogs={blogs} title="All Blogs!"/>
-}    </div>
+      {isLoading && <div>Loading..</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}{" "}
+    </div>
   );
 };
-
 export default Home;
