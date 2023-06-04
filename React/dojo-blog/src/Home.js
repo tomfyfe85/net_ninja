4cdcecb3590de284b333cfedc8c47b57a286d224
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-04 21:57:25 +0100

    15, useEffect with dependencies

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  // const [title, setTitle] = useState()
-
   const [blogs, setBlogs] = useState([
     { title: "my new website", body: "lorem..", author: "mario", id: 1 },
     { title: "Welcome party!", body: "orem", author: "yoshi", id: 2 },
     { title: "web dev to tips", body: "lorem", author: "mari0", id: 3 },
   ]);
+
+  const [name, setName] = useState("mario");
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -16,11 +16,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log("use effect ran");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 };
