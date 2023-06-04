292fbb80c57885653c05ab31c6bf0613423c8643
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-04 22:29:23 +0100

    17, addded fetch req to use effect. Removed delete button

--- React/dojo-blog/src/BlogList.js
+++ React/dojo-blog/src/BlogList.js
@@ -1,16 +1,11 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
-  
-
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
-      <h1>
-        {title}
-      </h1>
+      <h1>{title}</h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,26 +1,24 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "my new website", body: "lorem..", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "orem", author: "yoshi", id: 2 },
-    { title: "web dev to tips", body: "lorem", author: "mari0", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
 
   useEffect(() => {
-    console.log("use effect ran");
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-    </div>
+{  blogs &&  <BlogList blogs={blogs} title="All Blogs!"/>
+}    </div>
   );
 };
 
