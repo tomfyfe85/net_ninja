c5945b6a837c96c01a425e8c3e71877f1a6c8113
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-22 16:47:38 +0100

    26 - reusing the custom useFetch hook

--- React/dojo-blog/src/BlogDetails.js
+++ React/dojo-blog/src/BlogDetails.js
@@ -1,11 +1,23 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-
+import useFetch from "./useFetch";
 const BlogDetails = () => {
   const { id } = useParams();
-
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
   return (
-    <div>
-      <h2>Blog Details - {id}</h2>
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author} </p>
+          <p>{blog.body}</p>
+        </article>
+      )}
     </div>
   );
 };
