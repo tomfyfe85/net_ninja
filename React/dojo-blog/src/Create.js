2f2419ac429551332e16730f8644a9b2fc6a2b71
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-22 18:22:37 +0100

    28 - submitting a form

--- React/dojo-blog/src/Create.js
+++ React/dojo-blog/src/Create.js
@@ -4,11 +4,15 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
   return (
     <div className="create">
       <h2>Create a new BLOG</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title</label>
         <input
           type="text"
