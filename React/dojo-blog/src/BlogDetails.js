1b8f66d2c9a6c2a7839837712e659099d95ffbe8
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-22 19:25:54 +0100

    added delete button

--- React/dojo-blog/src/BlogDetails.js
+++ React/dojo-blog/src/BlogDetails.js
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
+
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -16,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author} </p>
           <p>{blog.body}</p>
+          <button onClick={handleClick}> delete</button>
         </article>
       )}
     </div>
