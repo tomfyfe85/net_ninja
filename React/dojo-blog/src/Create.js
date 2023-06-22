a6dee602688c64abdd36d342c8232a6dbaa877d5
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-22 18:42:38 +0100

    sending post requests

--- React/dojo-blog/src/Create.js
+++ React/dojo-blog/src/Create.js
@@ -4,10 +4,21 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
   };
   return (
     <div className="create">
@@ -33,7 +44,9 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Adding Blog...</button>}
+
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
