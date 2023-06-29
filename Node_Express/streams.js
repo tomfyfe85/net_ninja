5541c459089b606236938e1ce83e38a2ef72de14
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-29 13:00:02 +0100

    fs.createReadStream() & fs.createWriteStream()

--- Node_Express/streams.js
+++ Node_Express/streams.js
@@ -7,9 +7,13 @@ const readStream = fs.createReadStream("./docs/blog3.txt", {
 // where we are streaming from as argument
 // {encoding: 'utf8'} - meaning it will encode it to readable text as it comes in, otherwise  console.log(chunk.toString())
 
+const writeStream = fs.createWriteStream("./docs/blog4.txt");
+// will write each chunk received in the stream to a new file (blog4.txt)
 readStream.on("data", (chunk) => {
   console.log("---new chunk");
   console.log(chunk);
+  writeStream.write("\nNEW CHUNK\n");
+  writeStream.write(chunk);
 });
 // .on is an event listener
 // listening for a chunk of data in the buffer
