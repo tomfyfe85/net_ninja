af411d6eb693b6f280743f62b66403079f4b72e3
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-29 07:45:49 +0100

    test.js - examples of node.js

---
+++ Node_Express/test.js
@@ -0,0 +1,12 @@
+// const name = "mario";
+
+// console.log(name);
+
+const greet = (name) => {
+  setTimeout(() => {
+    console.log(`Hello ${name}`);
+  }, 3000);
+};
+
+greet("Tom");
+greet("Chants");
