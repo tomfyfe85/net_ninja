0a48ba9ed239476ab241f991880a7cc7164acab8
Tom Fyfe <tomfyfe85@gmail.com>
2023-06-29 11:46:46 +0100

    writting files

--- Node_Express/files.js
+++ Node_Express/files.js
@@ -2,11 +2,20 @@ const fs = require("fs");
 //  core module - file system
 
 // reading files
-fs.readFile("./docs/blog1.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
-});
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
 
-console.log("last line");
+// console.log("last line");
+
+//  writing files
+// fs.writeFile("./docs/blog1.txt", "hello world", () => {
+//   console.log("file was written");
+// });
+
+// fs.writeFile("./docs/blog2.txt", "hello world again", () => {
+//   console.log("file was written again");
+// });
