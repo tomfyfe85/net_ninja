ab35d215ca29c002608ef69f87b16f8ba92c46b5
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-03 00:51:08 +0100

    7 adding ejs and views

--- Node_Express/app.js
+++ Node_Express/app.js
@@ -2,24 +2,34 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
   // res.send("<p>home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  // res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
