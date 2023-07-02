7277acfd306b4cbf281d30228b209dd50b9b3c93
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-02 23:25:04 +0100

    added redirect and 404

--- Node_Express/app.js
+++ Node_Express/app.js
@@ -13,3 +13,13 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
 });
+
+// redirects
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
