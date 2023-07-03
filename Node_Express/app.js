8f7149e4b537387adfa9107fbf38305e9219ed76
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-03 11:20:46 +0100

    added middleware control logs

--- Node_Express/app.js
+++ Node_Express/app.js
@@ -5,8 +5,16 @@ const app = express();
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests 
+// listen for requests
 app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
+});
 
 app.get("/", (req, res) => {
   // res.send("<p>home page</p>");
@@ -47,3 +55,4 @@ app.use((req, res) => {
   // res.status(404).sendFile("./views/404.html", { root: __dirname });
   res.status(404).render("404", { title: 404 });
 });
+// use runs for every type of request
