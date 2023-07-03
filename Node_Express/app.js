a1c4bb316e54b00d3926eb96e9d2553c11c37725
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-03 01:05:21 +0100

    7 added titles using ejs

--- Node_Express/app.js
+++ Node_Express/app.js
@@ -11,16 +11,16 @@ app.listen(3000);
 app.get("/", (req, res) => {
   // res.send("<p>home page</p>");
   // res.sendFile("./views/index.html", { root: __dirname });
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
   // res.sendFile("./views/about.html", { root: __dirname });
-  res.render("about");
+  res.render("about", { title: "about" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create a new blog" });
 });
 
 // redirects
@@ -31,5 +31,5 @@ app.get("/blogs/create", (req, res) => {
 // 404 page
 app.use((req, res) => {
   // res.status(404).sendFile("./views/404.html", { root: __dirname });
-  res.status(404).render("404");
+  res.status(404).render("404", { title: 404 });
 });
